797be69f2eabb2b65be43152d90197bcc1c04427
Dinhtri Pham <phamdinhtriqs111@gmail.com>
2022-12-05 15:29:29 +0700

    scss

---
+++ .history/tailwind.config_20221205152806.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4575d8",
+        blueDark: "#00005C",
+        black: "#000",
+        dark: "#222222",
+        text1: "#474747",
+        text2: "#666666",
+        text3: "#666666",
+        white: "#fff",
+        error: "#f14550",
+      }
+    },
+  },
+  plugins: [],
+};
